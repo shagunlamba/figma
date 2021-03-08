436a141a0e841357a02602bd46112556821295ac
shagunlamba <lamba.shagun0@gmail.com>
2021-03-08 14:41:31 +0530

    setting directory

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    )
+}
+console.log("working");
+export default App;
